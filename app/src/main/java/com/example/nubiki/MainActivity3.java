b28293e571b8464b175ce37319d96ec2b26a8f22
user06 <45358>
2023-06-14 13:42:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/nubiki/MainActivity3.java
@@ -0,0 +1,20 @@
+package com.example.nubiki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+    public void startNewActivity(View v) {
+        Intent intent = new Intent(this, MainActivity4.class);
+        startActivity(intent);
+    }
+}
